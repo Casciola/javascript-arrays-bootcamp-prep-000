3889691bea77eed7caedd46b5c0a9fa9a432a101
Nick Casciola <s0948644@monmouth.edu>
2017-10-16 00:09:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,16 +2,16 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
   array = [element, ...array];
-}
+};
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
-}
+};
 
 function addElementToEndOfArray(array, element) {
   array = [...array, element];
-}
+};
 
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
-}
+};
